aaea44fa754524db339bf9b4b93375f76ce3aedd
Giovane <giovanefernandes35@gmail.com>
2021-11-17 16:02:06 -0300

    Add more examples

---
+++ exercise1.py
@@ -0,0 +1,15 @@
+
+# Develop an algorithm that asks the user for two integers. Print the sum of these two numbers on the screen #
+
+# -------------------------------------------------------------------------------------------------------------------- #
+
+name = input("Hello, tell us what's your name: ")
+print("All right, {}, let's make math accounts".format(name))
+
+number1 = int(input("Enter a number: "))
+number2 = int(input("Enter another number: "))
+
+sum = number1 + number2
+print("This is the sum of the two numbers you wrote: {}".format(sum))
+
+# -------------------------------------------------------------------------------------------------------------------- #
